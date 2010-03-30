617530642229e7505c5269e7c600625e7b286f09
Chris Hapgood <cch1@hapgoods.com>
2010-03-30 14:38:07 -0400

    Make helper use standard render options

--- lib/breadcrumbs.rb
+++ lib/breadcrumbs.rb
@@ -7,27 +7,26 @@ module Breadcrumbs
       url = send(url) if url.is_a?(Symbol)
       name = send(name).to_s.titleize if name.is_a?(Symbol)
       @breadcrumbs << [name, url]
-    end  
+    end
   end
-  
+
   module ClassMethods
     # Append a breadcrumb to the end of the trail by deferring evaluation until the filter processing.
     def add_breadcrumb(name, url = nil, options = {})
       before_filter(options) do |controller|
         controller.send(:add_breadcrumb, name, url)
       end
-    end    
+    end
   end
 
   module HelperMethods
     # Returns HTML markup for the breadcrumbs
-    def breadcrumbs(*args)
-      default_options = {:separator => "&nbsp;&raquo;&nbsp;"}
-      options = default_options.merge(args.extract_options!)
+    def breadcrumbs(options = {})
+      options = {:separator => "&nbsp;&raquo;&nbsp;", :collection => @breadcrumbs}.merge(options)
       if p = options[:partial]
-        render :partial => p, :collection => @breadcrumbs
+        render options
       else
-        @breadcrumbs.map do |name, url|
+        options[:collection].map do |name, url|
           crumb = link_to_unless_current(name, url)
           options[:tag] ? content_tag(options[:tag], crumb) : crumb
         end.join(options[:separator])
